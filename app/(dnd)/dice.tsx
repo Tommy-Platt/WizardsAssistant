672afd6092ad70de3bebe716b035ccc432e54421
Tommy-Platt <thomas.ct.platt@gmail.com>
2025-07-02 17:28:57 +1000

    Uploaded dice images
    Nothing added to the app, just need to move changes to the repo.

--- app/(dnd)/dice.tsx
+++ app/(dnd)/dice.tsx
@@ -1,7 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import useState from 'react'
 
 const Dice = () => {
+
+  const [modifier, setModifier]= useState();
+
   return (
     <View>
       <Text>D</Text>
